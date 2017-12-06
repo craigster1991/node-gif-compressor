ecd12f12088470266220512e0f266b5b04576781
Craig Wheatley <craigster1991@gmail.com>
2017-12-06 12:13:19 +0000

    added platform checking

--- services/compressorService.js
+++ services/compressorService.js
@@ -1,18 +1,35 @@
-const fs = require('fs');
-const path = require('path');
-const exec = require('child_process').exec;
+const fs = require('fs')
+const path = require('path')
+const exec = require('child_process').exec
+
+const compressIt = (data, resolve, reject) => {
+  const filePath = path.resolve(__dirname, `../uploads/${data.filename}`)
+  // const outputName = gs(data.filename).replace('.gif', '_compressed.gif')
+  const outputPath = path.resolve(__dirname, `../public/compressions/${data.filename}`)
+  const colours = data.colours == 'same' ? '' : `-k ${data.colours} `
+  exec(`./gifsicle -O3 --lossy=${data.compression} ${colours}-o ${outputPath} ${filePath}`, err => {
+    if (err) return reject(err)
+    return resolve(data.filename)
+  })
+}
 
 module.exports = {
   compressGif: data => {
     return new Promise((resolve, reject) => {
-      const filePath = path.resolve(__dirname, `../uploads/${data.filename}`)
-      // const outputName = gs(data.filename).replace('.gif', '_compressed.gif')
-      const outputPath = path.resolve(__dirname, `../public/compressions/${data.filename}`)
-      const colours = data.colours == 'same' ? '' : `-k ${data.colours} `
-      exec(`./gifsicle -O3 --lossy=${data.compression} ${colours}-o ${outputPath} ${filePath}`, err => {
-        if (err) return reject(err)
-        return resolve(data.filename)
-      })
+      switch(process.platform) {
+        case "darwin":
+          compressIt(data, resolve, reject)
+          break
+        case "win32":
+          reject('This only works on OSX currently!')
+          break
+        case "linux":
+          reject('This only works on OSX currently!')
+          break
+        default:
+          reject('This only works on OSX currently!')
+          break
+      }
     })
   },
   fileSize: file => {
