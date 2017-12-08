fd5bd194b7781be36df0f3914083cb827ee2e1f7
Craig Wheatley <craigster1991@hotmail.co.uk>
2017-12-08 10:03:40 +0000

    new binaries

--- services/compressorService.js
+++ services/compressorService.js
@@ -2,12 +2,14 @@ const fs = require('fs')
 const path = require('path')
 const exec = require('child_process').exec
 
-const compressIt = (data, resolve, reject) => {
+const compressIt = (data, platform, resolve, reject) => {
   const filePath = path.resolve(__dirname, `../uploads/${data.filename}`)
   // const outputName = gs(data.filename).replace('.gif', '_compressed.gif')
   const outputPath = path.resolve(__dirname, `../public/compressions/${data.filename}`)
   const colours = data.colours == 'same' ? '' : `-k ${data.colours} `
-  exec(`./gifsicle -O3 --lossy=${data.compression} ${colours}-o ${outputPath} ${filePath}`, err => {
+  exec(`./gifsicle -O3 --lossy=${data.compression} ${colours}-o ${outputPath} ${filePath}`, {
+    cwd: path.join(__dirname, `./${platform}`)
+  }, err => {
     if (err) return reject(err)
     return resolve(data.filename)
   })
@@ -18,16 +20,16 @@ module.exports = {
     return new Promise((resolve, reject) => {
       switch(process.platform) {
         case "darwin":
-          compressIt(data, resolve, reject)
+          compressIt(data, "darwin", resolve, reject)
           break
         case "win32":
-          reject('This only works on OSX currently!')
+          reject('You\'re platform, win32, is not supported!')
           break
         case "linux":
-          reject('This only works on OSX currently!')
+          compressIt(data, "linux", resolve, reject)
           break
         default:
-          reject('This only works on OSX currently!')
+          reject('You\'re platform is not supported!')
           break
       }
     })
